58a4c05d89647316ef412354db450386bac0a184
leedk <126259075+LKdiol@users.noreply.github.com>
2023-09-14 17:57:08 +0900

    Update VMExport2.sql

--- VMExport2.sql
+++ VMExport2.sql
@@ -1,13 +1,13 @@
 -- 딜리버리 그룹 또는 카탈로그 별로 검색하는 방식
 -- Copyright ⓒ Leedk. All rights reserved.
 -- Date 2023-09-14
-Use [2TEAMVDI]
+Use [SiteDB명]
 SELECT REPLACE(CONCAT('XDHyp:\Connections\',B.HypervisorConnectionName,REPLACE(C.RootPath, '/', '\'),'\',A.HostedMachineName,'.vm'), '\\','\') AS "[VirtualMachinePath]" , 
 CONCAT (A.Name , '$') AS "[ADComputerAccount]",
 REPLACE(A.AssociatedUserNames, ',', '; ') AS "[AssignedUsers]",
 CASE WHEN A.AgentVersion IS NULL OR A.AgentVersion = '' THEN 'Unknown' ELSE A.AgentVersion END AS "[VDAVersion]"
 , E.DisplayName AS "[CatalogsName]"
-FROM [2TEAMVDI].[MonitorData].[Machine] A
+FROM [MonitoringDB명].[MonitorData].[Machine] A
 LEFT OUTER JOIN [HostingUnitServiceSchema].[HypervisorConnection] B ON A.HypervisorId = B.HypervisorConnectionUid
 LEFT OUTER JOIN [HostingUnitServiceSchema].[HostingUnit] C ON B.HypervisorConnectionUid = C.HypervisorConnectionUid
 LEFT OUTER JOIN [chb_Config].[DesktopGroups] D ON A.DesktopGroupId = D.UUID
